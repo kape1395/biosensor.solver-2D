d0a887e3d777b5e39f968f703c7c9834e47d46d0
karolis <karolis@c479f540-251b-0410-b52c-ca35b89237be>
2008-11-02 10:39:35 +0000

    Biski zaidziu
    git-svn-id: https://localhost/svn/biosensor/solver-2D/trunk@48 c479f540-251b-0410-b52c-ca35b89237be

--- src/libbiosensor-slv-fd/bio/fd/SolverFactory.cxx
+++ src/libbiosensor-slv-fd/bio/fd/SolverFactory.cxx
@@ -9,7 +9,7 @@
  */
 BIO_SLV_NS::ISolver* BIO_SLV_FD_NS::SolverFactory::create(BIO_XML_NS::model::Model* model)
 {
-    log4cxx::LoggerPtr log(log4cxx::Logger::getLogger("libbiosensor"));
+    log4cxx::LoggerPtr log(log4cxx::Logger::getLogger("libbiosensor-slv-fd"));
     
     if (model == 0)
         throw bio::Exception("Model is NULL, no solver can be created.");
@@ -17,12 +17,12 @@ BIO_SLV_NS::ISolver* BIO_SLV_FD_NS::SolverFactory::create(BIO_XML_NS::model::Mod
     
     if (dynamic_cast<bio::xml::model::s::Explicit1D*>(&model->solver()) != 0)
     {
-        LOG4CXX_DEBUG(log, "Selected solver: bio::xml::model::s::Explicit1D");
+        LOG4CXX_INFO(log, "Selected solver: bio::xml::model::s::Explicit1D -> ???");
         throw bio::Exception("Not implemented: bio::xml::model::s::Explicit1D");
     }
     else if (dynamic_cast<bio::xml::model::s::Explicit2D*>(&model->solver()) != 0)
     {
-        LOG4CXX_DEBUG(log, "Selected solver: bio::xml::model::s::Explicit2D");
+        LOG4CXX_INFO(log, "Selected solver: bio::xml::model::s::Explicit2D -> BIO_SLV_FD_NS::ex2d::Solver");
         BIO_SLV_FD_NS::ex2d::Solver* solver = new BIO_SLV_FD_NS::ex2d::Solver(model);
         
         return solver;
