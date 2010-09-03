4a6c79d2b975531b08ea371f66fcf50515c9111a
Karolis Petrauskas <k.petrauskas@gmail.com>
2010-09-03 11:25:44 +0300

    Future improvements of adaptive time step.

--- src/libbiosensor-slv-fd/bio/slv/fd/im2d/ConstantCondition.cxx
+++ src/libbiosensor-slv-fd/bio/slv/fd/im2d/ConstantCondition.cxx
@@ -110,8 +110,11 @@ void BIO_SLV_FD_IM2D_NS::ConstantCondition::setConcentration(int x, double c)
 {
     if (std::abs(concentration - c) > ZERO_MAX)
     {
-        LOG_WARN(LOGGER << "Trying to set invalid concentration for a bound with constant condition: new=" << c << " const=" << concentration);
-        //throw BIO_NS::Exception("Trying to set concentration for a bound with constant condition...");
+        LOG_WARN(LOGGER
+                 << "Trying to set invalid concentration for a bound with constant condition:"
+                 << " new=" << c << " const=" << concentration
+                );
+        throw BIO_NS::Exception("Trying to set concentration for a bound with constant condition...");
     }
     edge->setC0(x, concentration);
 }
