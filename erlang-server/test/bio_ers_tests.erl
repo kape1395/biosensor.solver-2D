aa8612d092e028df99aed6313055c87682291c15
Karolis Petrauskas <k.petrauskas@omnitel.net>
2012-02-13 15:46:29 +0200

    Some improved tests.

--- erlang-server/test/bio_ers_tests.erl
+++ erlang-server/test/bio_ers_tests.erl
@@ -1,5 +1,36 @@
 -module(bio_ers_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-my_test() ->
-    ok.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  Test descriptions
+%%
+
+basic_test_() ->
+    {setup, fun start/0, fun stop/1, fun test_returns_string/1}.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  Setup functions
+%%
+
+start() ->
+    {ok, Pid} = bio_ers:start_link(),
+    Pid.
+
+stop(_Pid) ->
+    bio_ers:stop().
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  Actual tests
+%%
+
+test_returns_string(_Pid) ->
+    [?_assertEqual("This is a test", bio_ers:test())].
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%  Helper functions
+%%
+
+
